e2bc820a31ce2fdadf8b864369a645e4e0fe4b23
생이(SangE/남상이) <sy.nam@pet-friends.co.kr>
2024-06-30 22:08:26 +0900

    Revert "feat: openTelementry init"
    This reverts commit 9cb4cd28ad10dbcc72567bf30c64a034e260af7b.

--- src/instrumentation.ts
+++
@@ -1,8 +0,0 @@
-export async function register() {
-  if (process.env.NEXT_RUNTIME === "nodejs") {
-    console.info("register openTelementry");
-    await import("./instrumentation.node");
-  } else {
-    console.info("edge 환경임");
-  }
-}
